309d33055aadc4bb00ca65928a635ede467bc1f0
Chris Antos <sparrowhawk996@gmail.com>
2023-06-01 12:34:51 -0700

    Update version.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #define VERSION_MAJOR           0
-#define VERSION_MINOR           16
+#define VERSION_MINOR           17
 
 #define COPYRIGHT_STR           "Copyright (C) 2023 Christopher Antos"
 
