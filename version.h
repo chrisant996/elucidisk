7b6a8585726949b7f72e59cd834af1c8c2f0c0f5
Chris Antos <sparrowhawk996@gmail.com>
2025-05-18 12:25:24 -0700

    Increment version.

--- version.h
+++ version.h
@@ -4,9 +4,9 @@
 #pragma once
 
 #define VERSION_MAJOR           1
-#define VERSION_MINOR           3
+#define VERSION_MINOR           4
 
-#define COPYRIGHT_STR           "Copyright (C) 2023-2024 Christopher Antos"
+#define COPYRIGHT_STR           "Copyright (C) 2023-2025 Christopher Antos"
 
 #define IND_VER2( a, b ) #a ## "." ## #b
 #define IND_VER4( a, b, c, d ) L#a ## L"." ## L#b ## L"." ## L#c ## L"." ## L#d
